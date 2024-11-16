85f2da89daa3e0be4fc7b3b594720ce6a0eb4746
Panagiotis Panagiotopoulos <panagiotispanagiotopoulos2005@gmail.com>
2024-11-17 01:51:42 +0200

    Initial commit. A functional Windows shell with basic commands. It is inspired by Stephen Brennan's tutorial on writing a shell in C.

---
+++ README.md
@@ -0,0 +1,50 @@
+# Spit
+
+Spit is a simple command-line shell for Windows, inspired by the tutorial [Write a Shell in C](https://brennan.io/2015/01/16/write-a-shell-in-c/) by **Stephen Brennan**. I wanted to get familiar with the Windows API so I adjusted some of the functions, and I added some more functions.
+
+## Installation
+
+To install and set up the project, follow these steps:
+
+```bash
+# Clone the repository
+git clone https://github.com/yourusername/Spit.git
+
+# Navigate to the project directory
+cd Spit
+
+# Build the project using gcc
+gcc -o spit src/main.c src/lib/lib.c -luser32
+
+# Alternatively, you can use make
+make
+```
+
+## Usage
+
+To use the project, run the following command:
+
+```bash
+# Run the project
+./spit
+```
+
+## Contributing
+
+Guidelines for contributing to the project:
+
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature-branch`).
+3. Make your changes.
+4. Commit your changes (`git commit -m 'Add some feature'`).
+5. Push to the branch (`git push origin feature-branch`).
+6. Open a pull request.
+
+## License
+
+This project is licensed under the Unlicense - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgements
+
+- Special thanks to Mats Erik Andersson for creating lsh.
+- Special thanks to Stephen Brennan for his tutorial on writing a shell in C.
---
+++ src/main.c
@@ -0,0 +1,191 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <windows.h>
+
+#include "./lib/lib.h"
+#include "./lib/ansi_formats.h"
+
+#define SPIT_READLINE_BUFSIZE 1024
+#define SPIT_TOKEN_BUFSIZE 64
+#define SPIT_CWD_BUFSIZE 1024
+
+#define SPIT_TOKEN_DELIM " \t\r\n\a"
+
+
+void spit_loop(void);
+char *spit_read_line(void);
+char **spit_split_line(char *line);
+int spit_launch(char **args);
+int spit_execute(char **args);
+
+
+int main(int argc, char **argv) {
+    // Load necessary files
+
+    // Run command loop
+    spit_loop();
+    // Perform shutdown and cleanup
+    printf("Exiting...\n");
+    return EXIT_SUCCESS;
+}
+
+
+void spit_loop(void) {
+    char *line;
+    char **args;
+    int status;
+
+    char *cwd = malloc(SPIT_CWD_BUFSIZE);
+
+    do {
+        // Get the current working directory
+        if (!GetCurrentDirectory(SPIT_CWD_BUFSIZE, cwd)) {
+            fprintf(stderr, "spit: GetCurrentDirectory failed (%ld).\n", GetLastError());
+            exit(EXIT_FAILURE);
+        }
+
+        printf(BOLD_BRIGHT_YELLOW "%s>" RESET, cwd);
+        line = spit_read_line();
+        args = spit_split_line(line);
+        status = spit_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+char *spit_read_line(void) {
+    int bufsize = SPIT_READLINE_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "spit: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (true) {
+        // Read a character
+        c = getchar();
+
+        // If we hit EOF, replace it with a null character and return
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+
+        // If we exceed buffer size we need to reallocate
+        if (position >= bufsize) {
+            bufsize += SPIT_READLINE_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+
+            if (!buffer) {
+                fprintf(stderr, "spit: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+char **spit_split_line(char *line) {
+    int bufsize = SPIT_TOKEN_BUFSIZE;
+    int position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens) {
+        fprintf(stderr, "spit: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, SPIT_TOKEN_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += SPIT_TOKEN_BUFSIZE;
+            tokens = realloc(tokens, bufsize * (sizeof(char *)));
+
+            if (!tokens) {
+                fprintf(stderr, "spit: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, SPIT_TOKEN_DELIM);
+    }
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int spit_launch(char **args) {
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    DWORD status;
+
+    // Initialize memory for STARTUPINFO
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    // Count length of args
+    int length = 0;
+    for (int i = 0; args [i] != NULL; i++) {
+        length += strlen(args[i]) + 1;
+    }
+
+    // Convert args to a single command string for CreateProcess
+    char *command = malloc(length);
+    if (!command) {
+        fprintf(stderr, "spit: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+    command[0] = '\0';
+
+    // Concatenate args
+    for (int i = 0; args[i] != NULL; i++) {
+        strcat(command, args[i]);
+        strcat(command, " ");
+    }
+    // Create the process
+    if (!CreateProcess(NULL, command, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+        printf("spit: CreateProcess failed (%ld).\n", GetLastError());
+        free(command);
+        return 1;
+    }
+
+    // Wait for the child process to finish
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    // Get the exit status of the child process
+    GetExitCodeProcess(pi.hProcess, &status);
+
+    // Close handles to the process and thread
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    free(command);
+
+    return 1;
+}
+
+int spit_execute(char **args) {
+    int i;
+
+    if (args[0] == NULL) {
+        // An empty command was entered
+        return 1;
+    }
+
+    for (i = 0; i < spit_num_builtins(); i++) {
+        if (strcmp(args[0], builtin_str[i]) == 0) {
+            return (*builtin_func[i])(args);
+        }
+    }
+    return spit_launch(args);
+}
